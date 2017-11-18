a0958aa37051c8dc484592b394c6c9d864d8ed9b
Sean Cramsey <cramseys@oregonstate.edu>
2017-11-18 15:19:43 -0800

    css

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 	echo '<head>';
 	echo '<meta charset="UTF-8">';
 	echo '<title>Barfly Drink Database</title>';
-	/*echo '<link rel="stylesheet" href="http://web.engr.oregonstate.edu/~warrickc/cs290/css/index.css" />';*/
+	echo '<link rel="stylesheet" href="style.css" />';*/
 	echo '</head>';
 
 	echo '<nav>';
