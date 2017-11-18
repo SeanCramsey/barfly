2561a5ef5180141073ba15ad347eff2b1aebe3aa
Sean Cramsey <cramseys@oregonstate.edu>
2017-11-18 13:34:29 -0800

    Add connectvars

---
+++ connectvarsBARFLY.php
@@ -0,0 +1,7 @@
+<?php
+  // Define database connection constants
+  define('DB_HOST', 'classmysql.engr.oregonstate.edu');
+  define('DB_USER', 'cs340_warrickc');
+  define('DB_PASSWORD', '1ocWPAaeX0G#0y');
+  define('DB_NAME', 'cs340_warrickc');
+?>
