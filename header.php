80f8d7b27cbd3be2e92ae35fed2310e38717a28f
Sean Cramsey <cramseys@oregonstate.edu>
2017-11-18 15:17:45 -0800

    remove '/'

--- header.php
+++ header.php
@@ -8,10 +8,10 @@
 
 	echo '<nav>';
 		echo '<ul>';
-			echo '<li><a href="/home.php">Home</a></li>';
-			echo '<li><a href="/myacc.php">Account</a></li>';
-			echo '<li><a href="/drink.php">Drinks</a></li>';
-			echo '<li><a href="/bar.php">Bars</a></li>';
+			echo '<li><a href="home.php">Home</a></li>';
+			echo '<li><a href="myacc.php">Account</a></li>';
+			echo '<li><a href="drink.php">Drinks</a></li>';
+			echo '<li><a href="bar.php">Bars</a></li>';
   	echo '</ul>';
 	echo '</nav>';
 ?>
