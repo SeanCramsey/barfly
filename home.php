0a5aca29d97e0df63bf8f0681bd62c2c5bf5ce6b
David Okubo <okubod@oregonstate.edu>
2017-11-18 15:29:25 -0800

    add mockup
    added mockup stuff

--- home.php
+++ home.php
@@ -2,5 +2,22 @@
 <?php include('header.php'); ?>
 
 <body>
+<h1>HOME</h1>
+<div id="newsfeed">
+	<h5>News Feed<h5>
+	<p>Recent information about drinks go here</p>
+	<p>New drinks</p>
+	<p>New bars</p>
+	<p>Menu changes</p>
+</div>
+
+<div id="favorites">
+	<h5>Most Popular</h5>
+	<ul>
+		<li>Drink #1</li>
+		<li>Drink #2</li>
+		<li>Drink #3</li>
+	</ul>
+</div>
 
 </body>
