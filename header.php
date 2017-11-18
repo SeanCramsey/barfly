3866982b6c07041ae82672af2f7fae1abc74687a
warrickc <warrickc@oregonstate.edu>
2017-11-18 14:17:37 -0800

    Add files via upload

---
+++ header.php
@@ -0,0 +1,17 @@
+<?php
+
+	echo '<head>';
+	echo '<meta charset="UTF-8">';
+	echo '<title>Barfly Drink Database</title>';
+	/*echo '<link rel="stylesheet" href="http://web.engr.oregonstate.edu/~warrickc/cs290/css/index.css" />';*/
+	echo '</head>';
+
+	echo '<nav>';
+		echo '<ul>';
+			echo '<li><a href="http://web.engr.oregonstate.edu/~warrickc/">Home</a></li>';
+			echo '<li><a href="http://web.engr.oregonstate.edu/~warrickc/cs290/classes.html">Account</a></li>';
+			echo '<li><a href="http://web.engr.oregonstate.edu/~warrickc/cs290/aboutme.html">Drinks</a></li>';
+			echo '<li><a href="http://web.engr.oregonstate.edu/~warrickc/cs290/cs290resources.html">Bars</a></li>';
+  	echo '</ul>';
+	echo '</nav>';
+?>
