5dcd3a629635c229e2c354c71a23c4c07ecb6b8b
Curtis Warrick <warrickc@oregonstate.edu>
2017-11-18 16:38:36 -0800

    Additional header changes.

--- header.php
+++ header.php
@@ -1,17 +1,18 @@
 <?php
-
 	echo '<head>';
 	echo '<meta charset="UTF-8">';
 	echo '<title>Barfly Drink Database</title>';
-	echo '<link rel="stylesheet" href="style.css" />';
+	echo '<link rel="stylesheet" type="text/css" href="style.css"/>';
 	echo '</head>';
-
-	echo '<nav>';
-		echo '<ul>';
-			echo '<li><a href="home.php">Home</a></li>';
-			echo '<li><a href="myacc.php">Account</a></li>';
-			echo '<li><a href="drink.php">Drinks</a></li>';
-			echo '<li><a href="bar.php">Bars</a></li>';
-  	echo '</ul>';
-	echo '</nav>';
+	echo '<body>';
+	echo '	<header>';
+	echo '		<nav>';
+	echo '			<ul>';
+	echo '				<li><a href="home.php">Home</a></li>';
+	echo '				<li><a href="myacc.php">Account</a></li>';
+	echo '				<li><a href="drink.php">Drinks</a></li>';
+	echo '				<li><a href="bar.php">Bars</a></li>';
+  echo '			</ul>';
+	echo '		</nav>';
+	echo '	</header>';
 ?>
