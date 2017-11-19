be36c5ab59fb72e032ad33fe1515d73f8ee59f9b
Curtis Warrick <warrickc@oregonstate.edu>
2017-11-18 20:37:11 -0800

    Fixed merge issue, sorry!

--- home.php
+++ home.php
@@ -1,18 +1,6 @@
 <!DOCTYPE html>
 <html>
-<<<<<<< HEAD
-<?php include 'header.php'; ?>
-	<h1>HOME</h1>
-	<div id="newsfeed">
-		<h5>News Feed</h5>
-			<p>Recent information about drinks go here</p>
-			<p>New drinks</p>
-			<p>New bars</p>
-			<p>Menu changes</p>
-	</div>
-=======
 <?php include('header.php'); ?>
-
 <body>
 <h1>HOME</h1>
 <div id="newsfeed">
@@ -22,7 +10,6 @@
 	<p>New bars</p>
 	<p>Menu changes</p>
 </div>
-
 <div id="favorites">
 	<h5>Most Popular</h5>
 	<ul>
@@ -31,15 +18,5 @@
 		<li>Drink #3</li>
 	</ul>
 </div>
->>>>>>> 0a5aca29d97e0df63bf8f0681bd62c2c5bf5ce6b
-
-	<div id="favorites">
-		<h5>Most Popular</h5>
-		<ul>
-			<li>Drink #1</li>
-			<li>Drink #2</li>
-			<li>Drink #3</li>
-		</ul>
-	</div>
 </body>
 </html>
